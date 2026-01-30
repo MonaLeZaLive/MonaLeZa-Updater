9f65566e62447a9ee40b1ac24f8292256e2927a6
MonaLeZaLive <monalezalive@gmail.com>
2026-01-30 18:40:17 +0200

    Update updateMatches.js

--- updateMatches.js
+++ updateMatches.js
@@ -1,1 +1,21 @@
+/**
+ * ===============================
+ * Firebase Admin Initialization
+ * ===============================
+ */
+
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://monaleza-live-default-rtdb.firebaseio.com",
+});
+
+const db = admin.database();
+
+console.log("🔥 Firebase Admin Connected Successfully");
 
