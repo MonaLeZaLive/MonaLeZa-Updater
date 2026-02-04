939595b67e01fdf4d979d89d40bb1effc0284603
MonaLeZaLive <monalezalive@gmail.com>
2026-02-05 01:28:46 +0200

    Update livematches.js

--- livematches.js
+++ livematches.js
@@ -4,11 +4,11 @@ import axios from "axios";
 import dayjs from "dayjs";
 import admin from "firebase-admin";
 
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
 dayjs.extend(utc);
 dayjs.extend(timezone);
+
 
 
 /* ============================
